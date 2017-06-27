55fa483770e1ee732dc71c3c82ac37a037fa1435
ymartinez9415 <ymartinez9415@gmail.com>
2017-06-26 21:07:47 -0500

    Revert "add product class"
    This reverts commit adc952ea73472059d1f0b7a6d2adaaf3f3f0fa93.

--- Projects/AVC.ABase.Business/Model/Model_ABase.Designer.cs
+++ Projects/AVC.ABase.Business/Model/Model_ABase.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\Yilmar\Documents\AVC\AVC\Projects\AVC.ABase.Business\Model\Model_ABase.edmx'. 
+﻿// T4 code generation is enabled for model 'G:\Pedro\Personal\AVC\Projects\AVC.ABase.Business\Model\Model_ABase.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
